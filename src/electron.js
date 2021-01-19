20bf1111ec12ee02b018b72dddc5088405862d45
Harsh <harshpatel071@gmail.com>
2021-01-19 14:45:53 -0600

    Updated electron JS

--- src/electron.js
+++ src/electron.js
@@ -11,7 +11,6 @@ function   createWindow () {
     })
 
     win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname,"../build/index.html")}`)
-    win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
